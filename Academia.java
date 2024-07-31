5d77ba14914308ebce7fc0d1c48640462c4d8c94
joaoMarcelo <joaomarcelopudim@gmail.com>
2024-07-31 11:36:19 -0300

    yessir

---
+++ Academia.java
@@ -0,0 +1,32 @@
+public class Academia {
+    private String nome;
+    private String endereco;
+    private int telefone;
+
+    public Academia{
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    
+
+    
+}
